2705375472217eb1a9cec2ddd10560b1a2531d9f
scanlom <scanlom@asus-mint-18>
2018-08-12 22:48:22 +0800

    Move to api_, job_ naming convention, SQLAlchemy migration, fix imports

--- src/Kumamon/api_database.py
+++ src/Kumamon/api_database.py
@@ -2,6 +2,11 @@
 Created on Aug 11, 2018
 
 @author: scanlom
+
+Uses the SQLAlchemy ORM to access the finances database
+Notes
+1.  Callers are exposed to ORM objects
+2.  Callers do not access tables or SQLAlchemy objects directly
 '''
 from datetime import datetime
 from datetime import timedelta
@@ -21,26 +26,41 @@ class database2:
     CONST_INDEX_MANAGED     = 4
     CONST_INDEX_PLAY        = 5
     
+    CONST_PRICING_TYPE_BY_PRICE = 1
+    
     def __init__(self):
         self.Base = automap_base()
         self.engine = create_engine(config_database2_connect)
         self.Base.prepare(self.engine, reflect=True)
         self.session = Session(self.engine)
         self.IndexHistory = self.Base.classes.index_history
+        self.Constituents = self.Base.classes.constituents
+        self.Stocks = self.Base.classes.stocks
         
+    def commit(self):
+        return self.session.commit()
+    
     def get_index_row_minus_years(self, index, years):
         date = datetime.now() - timedelta(days=years*database2.CONST_DAYS_IN_YEAR)
         date = self.session.query(func.max(self.IndexHistory.date)).filter(self.IndexHistory.type == index, self.IndexHistory.date <= date).scalar()
-        return self.session.query(self.IndexHistory).filter(self.IndexHistory.type == index, self.IndexHistory.date == date).one();
+        return self.session.query(self.IndexHistory).filter(self.IndexHistory.type == index, self.IndexHistory.date == date).one()
+
+    def get_constituents(self, pricing_type):
+        return self.session.query(self.Constituents).filter(self.Constituents.pricing_type == pricing_type).all()
+    
+    def get_stocks(self):
+        return self.session.query(self.Stocks).all()
 
 def main():
     log.info("Started...")
     
     # Test
     db = database2()
-    row = db.get_index_row_minus_years(database2.CONST_INDEX_ROE, 12)
-    print(row.date)
-    print(row.value)
+    rows = db.get_constituents(database2.CONST_PRICING_TYPE_BY_PRICE)
+    print(rows[0].symbol)
+    
+    rows = db.get_stocks()
+    print(rows[0].symbol)
     
     log.info("Completed")
 
---
+++ src/Kumamon/job_price_value_update.py
@@ -0,0 +1,52 @@
+'''
+Created on Aug 7, 2013
+
+@author: scanlom
+'''
+
+from api_analytics import last
+from api_database import database2
+from api_log import log
+from api_mail import send_mail_html_self
+
+def get_market_data_symbol(symbol):
+    if symbol == "BRKB":
+        return "BRK-B"
+    return symbol
+
+def populate_price_and_value(db, rows, populate_value):
+    for row in rows:
+        log.info( "Downloading %s..." % ( row.symbol ) )
+        try:
+            row.price = last( get_market_data_symbol( row.symbol ) )
+            if populate_value:
+                row.value = row.price * row.quantity
+            log.info( "Updated %s..." % ( row.symbol ) )
+        except Exception as err:
+            log.error( "Could not get price for %s" % ( row.symbol ) )
+            log.exception(err)
+            continue    
+        
+    log.info( "Committing transaction..." )
+    db.commit()
+    log.info( "Done" )
+
+def main():
+    log.info("Started loading prices for constituents...")
+    db = database2()    
+    rows = db.get_constituents(db.CONST_PRICING_TYPE_BY_PRICE)   
+    populate_price_and_value(db, rows, True)
+    log.info("Completed")
+
+    log.info("Started loading prices for stocks...")
+    rows = db.get_stocks()   
+    populate_price_and_value(db, rows, False)
+    log.info("Completed")
+    
+if __name__ == '__main__':
+    try:
+        main()    
+    except Exception as err:
+        log.exception(err)
+        log.info("Aborted")
+        send_mail_html_self("FAILURE:  job_price_value_update.py", str( err ) ) 
