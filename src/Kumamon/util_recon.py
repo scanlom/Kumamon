78ba4f1741bda392937687826764d6904853563a
scanlom <scanlom@asus-mint-18>
2019-09-08 09:50:45 +0800

    Add managed cash

--- src/Kumamon/util_recon.py
+++ src/Kumamon/util_recon.py
@@ -21,9 +21,10 @@ def main():
         pos_owe_portfolio = db.get_balance_history(db.CONST_BALANCES_TYPE_OWE_PORTFOLIO, date)
         neg_cash_total = db.get_portfolio_history(db.CONST_PORTFOLIO_CASH, db.CONST_SYMBOL_CASH, date)
         neg_cash_self = db.get_portfolio_history(db.CONST_PORTFOLIO_SELF, db.CONST_SYMBOL_CASH, date)
-        recon = (pos_gs + pos_gs_ira + pos_gs_hkd + pos_owe_portfolio) - (neg_cash_total + neg_cash_self)
-        log.info("%s %s\t%s\t%s\t%s\t%s\t%s\t%s" % (date, recon, pos_gs, pos_gs_ira, pos_gs_hkd, pos_owe_portfolio, 
-                                                    neg_cash_total, neg_cash_self))
+        neg_cash_managed = db.get_portfolio_history(db.CONST_PORTFOLIO_MANAGED, db.CONST_SYMBOL_CASH, date)
+        recon = (pos_gs + pos_gs_ira + pos_gs_hkd + pos_owe_portfolio) - (neg_cash_total + neg_cash_self + neg_cash_managed)
+        log.info("%s %s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % (date, recon, pos_gs, pos_gs_ira, pos_gs_hkd, pos_owe_portfolio, 
+                                                    neg_cash_total, neg_cash_self, neg_cash_managed))
         date -= timedelta(1)    
     log.info("Completed")
 
