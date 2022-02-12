2241275698353c5de44427510944bd7b4b293c01
Mike Scanlon <scanlom@gmail.com>
2022-02-12 08:13:11 +0800

    Blue Lion Bridge - Copied Researches

--- src/Kumamon/job_blue_lion_bridge.py
+++ src/Kumamon/job_blue_lion_bridge.py
@@ -16,33 +16,26 @@ def main():
 
     db = database2()    
     rows = db.get_researches()
+    f = open("tmp.txt", "w")
+    
     for r in rows:
         p = _abl.projections_by_symbol(r.symbol)
         ref = _abl.ref_data_by_symbol(r.symbol)
         if p is None or p['id'] == 0:
+            print("No projections found for " + r.symbol)
             continue # will handle manually
         confidence = _abl.confidence(r.comment)[0]
         eps_yr1 = r.eps_yr1 if r.eps_yr1 is not None else 0
         eps_yr2 = r.eps_yr2 if r.eps_yr2 is not None else 0
+        comment = r.comment.replace("'", "''")
 
-        print( """INSERT INTO public.projections_journal(
+        f.write("""INSERT INTO public.projections_journal(
         projections_id, ref_data_id, date, eps, dps, growth, pe_terminal, payout, book, roe, eps_yr1, eps_yr2, confidence, ticker, description, 
-        sector, industry, price, div_plus_growth, eps_yield, dps_yield, cagr_5yr, cagr_10yr, croe_5yr, croe_10yr, entry)
-        VALUES (%d, %d, %s, %f, %f, %f, %d, %f, %f, %f, %f, %f, %s, %s, %s, %s, %s, %f, %f, %f, %f, %f, %f, %f, %f, %s);""" % (p['id'], ref['id'], r.date.strftime('%Y-%m-%d'), r.eps, r.div, r.growth, r.pe_terminal, r.payout, r.book, r.roe, eps_yr1, eps_yr2, 
-        confidence, ref['symbol'], ref['description'], ref['sector'], ref['industry'], r.price, r.div_plus_growth, r.eps_yield, r.div_yield,
-        r.five_year_cagr, r.ten_year_cagr, r.five_year_croe, r.ten_year_croe, r.comment) )
+        sector, industry, price, div_plus_growth, eps_yield, dps_yield, cagr_5yr, cagr_10yr, croe_5yr, croe_10yr, entry, eps_cagr_5yr, eps_cagr_10yr, pe_high_mmo_5yr, pe_low_mmo_5yr, roe_5yr, pe, eps_cagr_2yr, eps_cagr_7yr, magic)
+        VALUES (%d, %d, '%s', %f, %f, %f, %d, %f, %f, %f, %f, %f, '%s', '%s', '%s', '%s', '%s', %f, %f, %f, %f, %f, %f, %f, %f, '%s',0,0,0,0,0,0,0,0,0);\n""" % (p['id'], ref['id'], r.date.strftime('%Y-%m-%d'), r.eps, r.div, r.growth, r.pe_terminal, r.payout, r.book, r.roe, eps_yr1, eps_yr2, confidence, ref['symbol'], ref['description'], ref['sector'], ref['industry'], r.price, r.div_plus_growth, r.eps_yield, r.div_yield, r.five_year_cagr, r.ten_year_cagr, r.five_year_croe, r.ten_year_croe, comment) )
 
+    f.close()
     log.info("Completed")
-
-    """Then have to clean up
-    WYN
-    CBG
-    SAN.PA
-    CNSWF
-    CBG
-    CBS
-    NSRGY
-    """
 
 if __name__ == '__main__':
     try:
